5c2875c3f9fc1f50156944bdfbc6b3eb43c6a664
kkimjuseong <rofodzl1@naver.com>
2024-04-08 17:49:16 +0900

    수정사항

--- js/app2048.js
+++ js/app2048.js
@@ -9,7 +9,7 @@ const columns = 4;
 const $bestScore = document.getElementById("bestScore");
 // const $score = document.getElementById("score");
 // const $board = document.getElementById("board");
-
+const $button = document.getElementById("btnArea button");
 
 window.onload = function () {
   setGame();
@@ -25,41 +25,41 @@ function setGame() {
     [0, 0, 0, 0],
   ];
 
-// 타일 생성과 초기 상태 설정
-// 4x4 크기의 보드를 생성하기 위해 이중반복문 사용
-// 행을 반복
-for (let r = 0; r < rows; r++) {
-  // 열을 반복
-  for (let c = 0; c < columns; c++) {
-    // 새로운 div 요소를 생성하여 타일을 나타냄
-    let tile = document.createElement("div");
-    // 타일의 id를 행과 열을 이용하여 지정
-    // 즉 id=tile를 만들고 이를 표시하는 id명이
-    // 0-1 0-2 0-3 0-4
-    // 1-0 1-1 1-2 1-3
-    // 2-0 2-1 2-2 2-3
-    // 3-0 3-1 3-2 3-3 처럼 표현됨
-    tile.id = r.toString() + "-" + c.toString();
+  // 타일 생성과 초기 상태 설정
+  // 4x4 크기의 보드를 생성하기 위해 이중반복문 사용
+  // 행을 반복
+  for (let r = 0; r < rows; r++) {
+    // 열을 반복
+    for (let c = 0; c < columns; c++) {
+      // 새로운 div 요소를 생성하여 타일을 나타냄
+      let tile = document.createElement("div");
+      // 타일의 id를 행과 열을 이용하여 지정
+      // 즉 id=tile를 만들고 이를 표시하는 id명이
+      // 0-1 0-2 0-3 0-4
+      // 1-0 1-1 1-2 1-3
+      // 2-0 2-1 2-2 2-3
+      // 3-0 3-1 3-2 3-3 처럼 표현됨
+      tile.id = r.toString() + "-" + c.toString();
 
-    // 현재 타일의 숫자 값을 가져옴
-    let num = board[r][c];
-    // updateTile 함수를 사용하여 타일을 업데이트함
-    updateTile(tile, num);
-    // 게임 보드의 HTML 요소에 생성된 타일을 추가
-    document.getElementById("board").append(tile);
+      // 현재 타일의 숫자 값을 가져옴
+      let num = board[r][c];
+      // updateTile 함수를 사용하여 타일을 업데이트함
+      updateTile(tile, num);
+      // 게임 보드의 HTML 요소에 생성된 타일을 추가
+      document.getElementById("board").append(tile);
+    }
   }
-}
 
   // 게임 시작 시에 두 개의 타일 생성
   setTwo();
   setTwo();
 }
 
-function setTwo(){
+function setTwo() {
   // 빈 공간을 확인 , 빈 공간, 랜덤한 위치에 새로운 타일을 생성해야함
 
   // 빈 공간이 없으면 게임이 종료되는 로직도 넣어줘야함
-  // 처음 checkEmptyTiles() 이 상태에서 게임 종료를 만들었으나, 
+  // 처음 checkEmptyTiles() 이 상태에서 게임 종료를 만들었으나,
   // 이때 타일이 꽉찼을때 결합할 수 있는 타일이 있는데 종료가 되는 상황이생겼음
   // 그래서 이동을 할 수 없을때 조건을 추가해야 겠다 생각을 함
   // 이후 checkGameEndCondition함수를 만들어 넣었더니 예상대로 됨.
@@ -77,24 +77,24 @@ function setTwo(){
 
   // 초기 시작은 빈 타일이 있는지 없는지 알 수 없으므로 false 찾지못했다라 가정함
   let found = false;
-  
+
   // 처음 시작은 whil(true)로 시작하였음 그러나 타일이 생성되지 않았고,
   // 이유를 찾던 중 타일이 새로 생성되면 반복문을 빠져 나가야 한다는걸 알게됨
   // 그래서 found를 false로 정의해두고 시작함
 
   // while(true) 로 만들지 않은 이유는 빈 공간을 찾지 않더라도 계속 진행돼
   // 무한루프에 빠지게 될 수 있음 그래서 빈 공간을 찾은 경우에만 반복을 멈추도록
-  // found변수를 사용하는것이 좋음 
-  while(!found){
+  // found변수를 사용하는것이 좋음
+  while (!found) {
     let r = Math.floor(Math.random() * rows);
     let c = Math.floor(Math.random() * columns);
-    if(board[r][c] === 0){
+    if (board[r][c] === 0) {
       // 빈 공간에 2 생성
       board[r][c] = 2;
       // 해당 위치에 새로운 타일이 생성
       let $tile = document.getElementById(r.toString() + "-" + c.toString());
-      $tile.innerHTML = '2';
-      $tile.classList.add('x2');
+      $tile.innerHTML = "2";
+      $tile.classList.add("x2");
       found = true;
     }
   }
@@ -102,10 +102,10 @@ function setTwo(){
 
 // 빈 공간이 있는지 확인하는 함수 빈 타일이 있으면 게임이 종료되지 않음
 // 여기에 alert을 넣었는데 이러면 게임이 끝날때가 아니라 이동방향이 잘못되었을때도 뜨게됨 고쳐야해
-function checkEmptyTiles(){
-  for(let r = 0; r < rows; r++){
-    for(let c = 0; c < columns; c++){
-      if(board[r][c] === 0){
+function checkEmptyTiles() {
+  for (let r = 0; r < rows; r++) {
+    for (let c = 0; c < columns; c++) {
+      if (board[r][c] === 0) {
         return true;
       }
     }
@@ -118,13 +118,12 @@ function checkEmptyTiles(){
 // 그래서 true로 바꿔주고 호출부에서 !hasEmptyTile 부정을 해주니 새로고침이 됨
 // 이게 왜 이럴까? 설명이 필요해보임
 
-
 // 타일 이동 및 결합 로직 구현
 // 이해하고 나니 코드 자체는 쉬웠는데 어떤식으로 만들어야 할지 몰라 어려웠다
-function slide(row){
-  row = filterZero(row); 
-  for (let i = 0; i < row.length - 1; i++){
-    if(row[i] === row[i + 1]){
+function slide(row) {
+  row = filterZero(row);
+  for (let i = 0; i < row.length - 1; i++) {
+    if (row[i] === row[i + 1]) {
       row[i] *= 2;
 
       // 같은숫자가 합쳐지면 한개만 남고 하나는 사라져야 하기 때문에
@@ -133,19 +132,19 @@ function slide(row){
       score += row[i];
     }
   }
-  while(row.length < columns){
+  while (row.length < columns) {
     row.push(0);
   }
   return row;
 }
 
 function filterZero(row) {
-// 이 함수는 주어진 배열에서 0이 아닌 값들만을 추출하여 새로운 배열을 생성 및 반환
-// 이동 후에 0이 아닌 값들만을 남기는 데에 활용됩니다.
-  return row.filter((num) => num !== 0); 
+  // 이 함수는 주어진 배열에서 0이 아닌 값들만을 추출하여 새로운 배열을 생성 및 반환
+  // 이동 후에 0이 아닌 값들만을 남기는 데에 활용됩니다.
+  return row.filter((num) => num !== 0);
 }
 
-function slideLeft(){
+function slideLeft() {
   for (let r = 0; r < rows; r++) {
     let row = board[r]; // [0, 2, 2, 2]
     row = slide(row);
@@ -153,7 +152,7 @@ function slideLeft(){
     for (let c = 0; c < columns; c++) {
       let tile = document.getElementById(r.toString() + "-" + c.toString());
       let num = board[r][c];
-      updateTile(tile, num)
+      updateTile(tile, num);
     }
   }
 }
@@ -172,7 +171,6 @@ function slideRight() {
     }
   }
 }
-
 
 // slideUp() 함수는 게임 보드를 위쪽으로 이동시키고 결합하는 역할을 함
 function slideUp() {
@@ -205,16 +203,16 @@ function slideDown() {
 }
 
 // 업데이트 타일
-function updateTile(tile, num){
+function updateTile(tile, num) {
   tile.innerHTML = "";
   tile.classList.value = "";
-  tile.classList.add('tile');
-  if(num > 0){
+  tile.classList.add("tile");
+  if (num > 0) {
     tile.innerText = num.toString();
-    if(num <= 4096){
+    if (num <= 4096) {
       tile.classList.add("x" + num.toString());
-    }else{
-      tile.classList.add('x8192')
+    } else {
+      tile.classList.add("x8192");
     }
   }
 }
@@ -226,7 +224,7 @@ function checkGameEndCondition() {
     for (let c = 0; c < columns; c++) {
       // 현재 위치의 타일 값
       let currentTile = board[r][c];
-      
+
       // 왼쪽 방향으로 인접한 타일과 비교하여 이동 가능한지 확인
       // c > 0 은 c열이 보드의 가장 왼쪽열에 위치하지 않는지 판단.
       if (c > 0 && (board[r][c - 1] === 0 || board[r][c - 1] === currentTile)) {
@@ -234,17 +232,23 @@ function checkGameEndCondition() {
       }
 
       // 오른쪽 방향으로 인접한 타일과 비교하여 이동 가능한지 확인
-      if (c < columns - 1 && (board[r][c + 1] === 0 || board[r][c + 1] === currentTile)) {
+      if (
+        c < columns - 1 &&
+        (board[r][c + 1] === 0 || board[r][c + 1] === currentTile)
+      ) {
         return false;
       }
-      
+
       // 위쪽 방향으로 인접한 타일과 비교하여 이동 가능한지 확인
       if (r > 0 && (board[r - 1][c] === 0 || board[r - 1][c] === currentTile)) {
         return false;
       }
 
       // 아래쪽 방향으로 인접한 타일과 비교하여 이동 가능한지 확인
-      if (r < rows - 1 && (board[r + 1][c] === 0 || board[r + 1][c] === currentTile)) {
+      if (
+        r < rows - 1 &&
+        (board[r + 1][c] === 0 || board[r + 1][c] === currentTile)
+      ) {
         return false;
       }
     }
@@ -253,12 +257,11 @@ function checkGameEndCondition() {
   return true;
 }
 
-
-// 사용자 입력처리 는 ketup 이벤트를 사용 
+// 사용자 입력처리 는 ketup 이벤트를 사용
 // 키를 입력한 경우 숫자가 합쳐지기 때문에 여기에 score를 넣어줘야 하나?
 // if문을 돌려서 이벤트를 진행하고 if문이 끝났을때 score를 넣어주면 될듯
 
-// code 이벤트객체 속성을 사용 
+// code 이벤트객체 속성을 사용
 // 아스키코드인 ArrowLeft를 써서 왼쪽키임을 입력
 // 계속 테스트를 하던 중 오른손가락 아파서 왼손으로 방향키를 눌렀으나 불편하여
 // wasd 를 넣어주자 생각이 듬.
@@ -281,6 +284,14 @@ document.addEventListener("keyup", (e) => {
     setTwo();
   }
   document.getElementById("score").innerText = score;
+
+
+  $button.classList.add("pressed"); // 버튼이 눌렸을 때 효과 클래스 추가
+  setTimeout(() => {
+    $button.classList.remove("pressed"); // 0.1초 후 효과 클래스 제거
+  }, 100);
+
+
 });
 
 document.getElementById("Up").addEventListener("click", () => {
@@ -302,7 +313,6 @@ document.getElementById("Right").addEventListener("click", () => {
   slideRight();
   setTwo();
 });
-
 
 /*
 전역변수 설정: 
